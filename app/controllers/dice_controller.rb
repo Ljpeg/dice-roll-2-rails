17c499113f2ec0401c64111fd675c5c9e0a92b48
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2025-09-16 22:05:22 +0000

    adds rcav for 1d20

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -3,4 +3,9 @@ class DiceController < ApplicationController
   def five_four
     render({:template => "dice_templates/five_d_four"})
   end 
+
+  def one_twenty
+    render({:template => "dice_templates/one_d_twenty"})
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
    get("/dice/5/4", { :controller => "dice", :action => "five_four" })
+
+   get("dice/1/20", { :controller => "dice", :action => "one_twenty"})
 end
